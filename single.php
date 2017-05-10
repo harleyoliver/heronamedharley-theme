79869e8ad7d86c39ddfa031f182f92ba5cdc0685
harleyoliver <hrlyolvr@gmail.com>
2017-05-10 18:25:33 +1000

    Button fixes and date added to blog posts

--- single.php
+++ single.php
@@ -11,15 +11,16 @@ get_header();
 		<div class="container">
 			<div class="row">
 				<main class="twelve col offset-2" role="main">
-					<h1><?php the_title(); ?></h1>
-					<?php
-					if ( have_posts() ) :
-						while ( have_posts() ) :
-							the_post();
-							the_content();
-						endwhile;
-					endif;
-					?>
+				<h1><?php the_title(); ?></h1>
+				<small><?php the_time( 'd/m/y' ); ?></small>
+				<?php
+				if ( have_posts() ) :
+					while ( have_posts() ) :
+						the_post();
+						the_content();
+					endwhile;
+				endif;
+				?>
 				</main>
 			</div>
 		</div>
